647e320ab19dfca934cd5a09b4a91dd3b139f0bb
Nehal Patel <patelnehal4u@gmail.com>
2016-12-29 16:51:15 +0530

    working chat application

--- app.js
+++ app.js
@@ -6,23 +6,47 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+/*app.get('/chat', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});*/
+
+users = [];
 io.on('connection', function(socket){
-  console.log('a user connected');
+
+  socket.is_logged = false;
+  
+  socket.on('setUsername', function(data){
+    if(users.indexOf(data) == -1){
+      users.push(data);
+      socket.emit('userSet', {username: data});
+      socket.username = data;
+      socket.is_logged = true;
+
+      socket.emit('join', 'good morning ' + socket.username);
+      //this event will broadcast the event to all other sockets except newly connected client
+      socket.broadcast.emit('newUser', socket.username + ' join room');
+
+    } else {
+      console.log(data + ' username is taken! Try some other username.');
+      socket.emit('userExists', data + ' username is taken! Try some other username.');
+    }
+
+  });
   
   socket.on('disconnect', function(){
     console.log('user disconnected');
-    socket.broadcast.emit('newclientconnect',{ description: 'a client left the room'})
+
+    var index = users.indexOf(socket.username);
+    if (index > -1) {
+      users.splice(index, 1);
+      console.log(socket.username + " removed from chat");
+    }
+    socket.broadcast.emit('message',{ message: socket.username + ' left the room'});
   });
 
   socket.on('message', function(msg){
-    io.emit('message', msg);
+    io.emit('message', {message: socket.username +" : "+ msg});
   });
-
-  //this event will be emit to the newly connected client ( .on('connection') )
-  socket.emit('newclientconnect',{ description: 'Hey, welcome!'});
-
-  //this event will broadcast the event to all other sockets except newly connected client
-  socket.broadcast.emit('newclientconnect',{ description: 'new client connected!'});
  
 });
 
